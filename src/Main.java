2488750e293735a5c527e734441b1b78dadf603e
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-20 14:34:25 +0900

    Runnable array sum

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
+class SumRunnable implements Runnable{
+    private final int[] numbers;
+
+    public SumRunnable(int[] numbers){
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run(){
+        int sum = 0;
+
+        for(int n : numbers){
+            sum += n;
+
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                System.out.println("인터럽트 발생");
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName() + " - Sum: " + sum);
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        new Thread(()-> {
-            System.out.println("Runnable 실행 중");
+        int[][] dataSets = {
+                {1, 2, 3, 4, 5},
+                {10, 20, 30},
+                {7, 14, 21, 28},
+                {100, 200, 300, 400}
+        };
 
-            for(int i = 1; i <= 5; i++) {
-                try {
-                    Thread.sleep(5000);
+        for (int i = 0; i < dataSets.length; i++){
+            Thread sumThread = new Thread(new SumRunnable(dataSets[i]));
 
-                    System.out.println(i);
-                } catch(InterruptedException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }).start();
+            sumThread.start();
+        }
+
+        System.out.println("All threads started");
     }
 }
