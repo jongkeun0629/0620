064f6fdf5bdb346d5ac373bdd0da1f7ba34e64c3
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-20 10:25:51 +0900

    Stream/for average

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Double> values = Arrays.asList(10.0, 20.0, 30.0);
+
+        double result = values.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0); // 안전장치
+
+        System.out.println(result);
+
+        // for
+        double sum = 0.0;
+        for (Double v : values){
+            sum += v;
+        }
+        double avg = sum / values.toArray().length;
+
+        System.out.println(avg);
     }
 }
